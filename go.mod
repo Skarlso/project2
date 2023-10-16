642bb996ed660a6e6d8504c758f0b52d5932b307
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2023-10-16 11:41:42 +0200

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/skarlso/project2
+
+go 1.21.2
+
+require github.com/skarlso/dependency/v2 v2.0.0
---
+++ main.go
@@ -0,0 +1,9 @@
+package project2
+
+import (
+	"github.com/skarlso/dependency/v2"
+)
+
+func Project2() string {
+	return dep.Test()
+}
